9b4a34b9b66a8ef3713dd26a48da156f08a39452
Aaron Li <li.yuxin@northeastern.edu>
2024-04-10 18:58:51 -0400

    loading spinner implemented

--- src/components/Course/AddCourse/AddCourseModal.tsx
+++ src/components/Course/AddCourse/AddCourseModal.tsx
@@ -6,6 +6,7 @@ import GenericModal from "../../Generic/GenericModal";
 import GenericButton from "../../Generic/GenericButton";
 import AddCourseDisplay from "./AddCourseDisplay";
 import { alreadyExists } from "../../../common/utils";
+import ClipLoader from 'react-spinners/ClipLoader';
 
 interface AddCourseModalProps {
   campus: Campus;
@@ -41,7 +42,9 @@ const AddCourseModal: React.FC<AddCourseModalProps> = ({
         <SearchCoursesInput
           setSearchQuery={setSearchQuery}
         ></SearchCoursesInput>
-        {isLoading && <div>Loading...</div>}
+        <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100%' }}>
+          {isLoading && <ClipLoader color="blue" size={35} />}
+        </div>
         {error && <div>Trouble finding classes!</div>}
         {searchQuery &&
           courses &&
