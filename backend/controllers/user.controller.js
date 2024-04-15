9b9680195c39f76b09ccad3272bfab0237a282a0
Aaron Li <li.yuxin@northeastern.edu>
2024-04-15 12:19:06 -0400

    User to user

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -1,4 +1,4 @@
-import User from "../models/User.js";
+import User from "../models/user.js";
 
 async function addPlan(req, res) {
   const { uuid, plan } = req.body;
--- backend/routes/session.routes.js
+++ backend/routes/session.routes.js
@@ -1,5 +1,5 @@
 import express from "express";
-import User from "../models/User.js";
+import User from "../models/user.js";
 import { v4 as uuidv4 } from "uuid";
 
 const router = express.Router();
