310e171fb2097f90f20f13dd05de2b96b26098ad
Aaron Li <li.yuxin@northeastern.edu>
2024-04-16 00:53:13 -0400

    newest

--- backend/app.js
+++ backend/app.js
@@ -9,11 +9,7 @@ const app = express();
 dotenv.config();
 
 app.use(express.json());
-app.use(
-  cors({
-    origin: "http://localhost:3000",
-  })
-);
+app.use(cors());
 
 app.use("/api/session", sessionRouter);
 app.use("/api/plans", planRoutes);
